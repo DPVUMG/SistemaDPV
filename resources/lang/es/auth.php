b09d32a32cc25a66b81aaf44d015fe926e882434
elioOjeda <elio4392@gmail.com>
2022-10-25 22:08:26 -0600

    Cambios

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Las credenciales ingresadas no son correctas o su usuario fue desactivado.',
+    'throttle' => 'A realizado la cantidad de intentos de acceso al sistema, espera 1 minutos y vuelve a intentar.',
+
+];
