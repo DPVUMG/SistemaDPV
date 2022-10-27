229003502752f63659fcdeadc1b61fe1dbad89cd
elioOjeda <elio4392@gmail.com>
2022-10-26 19:45:34 -0600

    Cambios

--- resources/views/sistema/adminstracion_escuela/descuento/grid.blade.php
+++ resources/views/sistema/adminstracion_escuela/descuento/grid.blade.php
@@ -27,6 +27,10 @@
                             <th colspan="2" class="text-center align-middle">Información</th>
                             <th colspan="2" class="text-center align-middle">Precio</th>
                             <th rowspan="2" class="text-center align-middle">Registrado</th>
+                            <th rowspan="2" class="text-center align-middle">
+                                <img src="{{ asset('image/ico_opcion.png') }}" title="Opciones" height="20px"
+                                    alt="Opciones">
+                            </th>
                         </tr>
                         <tr>
                             <th class="text-center align-middle">Código</th>
@@ -64,78 +68,7 @@
 
                             <td class="text-center align-middle">{{ date('d/m/Y H:i:s', strtotime($item->created_at))
                                 }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div id="tabs-2">
-        <div class="card">
-            <div class="card-header card-header-info">
-                <h4 class="card-title ">
-                    {{ __('Descuentos históricos') }}
-                </h4>
-                @if (is_null($establecimiento))
-                <p class="card-category"> {{ __("En esta pantalla el sistema muestra todos los descuentos
-                    registrados en el sistema.") }}</p>
-                @else
-                <p class="card-category"> {{ __("En esta pantalla el sistema muestra todos los descuentos
-                    registrados en la escuela {$establecimiento}.") }}</p>
-                @endif
-            </div>
-            <div class="card-body">
-                <table class="dataTable display" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th rowspan="2" class="text-center align-middle">Escuela</th>
-                            <th rowspan="2" class="text-center align-middle">Fotografía</th>
-                            <th colspan="3" class="text-center align-middle">Producto</th>
-                            <th colspan="2" class="text-center align-middle">Información</th>
-                            <th colspan="2" class="text-center align-middle">Precio</th>
-                            <th rowspan="2" class="text-center align-middle">Registrado</th>
-                            <th rowspan="2" class="text-center align-middle">
-                                <img src="{{ asset('image/ico_opcion.png') }}" title="Opciones" height="20px"
-                                    alt="Opciones">
-                            </th>
-                        </tr>
-                        <tr>
-                            <th class="text-center align-middle">Código</th>
-                            <th class="text-center align-middle">Nombre</th>
-                            <th class="text-center align-middle">Descripción</th>
 
-                            <th class="text-center align-middle">Variante</th>
-                            <th class="text-center align-middle">Presentacion</th>
-
-                            <th class="text-center align-middle">Precio Real</th>
-                            <th class="text-center align-middle">Precio Descuento</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($data->filter(function($item) {
-                        return !$item->activo;
-                        })->all() as $item)
-                        <tr>
-                            <td class="text-left align-middle">{{ $item->escuela->establecimiento }}</td>
-
-                            <td class="text-center align-middle">
-                                <img src="{{ $item->producto->getPictureAttribute() }}"
-                                    alt="{{ $item->producto->codigo }}" width="100px" class="img img-thumbnail">
-                            </td>
-
-                            <td class="text-center align-middle">{{ $item->producto->codigo }}</td>
-                            <td class="text-left align-middle">{{ $item->producto->nombre }}</td>
-                            <td class="text-left align-middle">{!! $item->producto->descripcion !!}</td>
-
-                            <td class="text-center align-middle">{{ $item->variante->nombre }}</td>
-                            <td class="text-center align-middle">{{ $item->presentacion->nombre }}</td>
-
-                            <td class="text-right align-middle">{{ number_format($item->precio_original,2) }}</td>
-                            <td class="text-right align-middle">{{ number_format($item->precio,2) }}</td>
-
-                            <td class="text-center align-middle">{{ date('d/m/Y H:i:s', strtotime($item->created_at))
-                                }}</td>
 
                             <td class="text-center align-middle">
                                 @if (is_null($establecimiento))
@@ -174,6 +107,74 @@
                                 </form>
                                 @endif
                             </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div id="tabs-2">
+        <div class="card">
+            <div class="card-header card-header-info">
+                <h4 class="card-title ">
+                    {{ __('Descuentos históricos') }}
+                </h4>
+                @if (is_null($establecimiento))
+                <p class="card-category"> {{ __("En esta pantalla el sistema muestra todos los descuentos
+                    registrados en el sistema.") }}</p>
+                @else
+                <p class="card-category"> {{ __("En esta pantalla el sistema muestra todos los descuentos
+                    registrados en la escuela {$establecimiento}.") }}</p>
+                @endif
+            </div>
+            <div class="card-body">
+                <table class="dataTable display" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th rowspan="2" class="text-center align-middle">Escuela</th>
+                            <th rowspan="2" class="text-center align-middle">Fotografía</th>
+                            <th colspan="3" class="text-center align-middle">Producto</th>
+                            <th colspan="2" class="text-center align-middle">Información</th>
+                            <th colspan="2" class="text-center align-middle">Precio</th>
+                            <th rowspan="2" class="text-center align-middle">Registrado</th>
+                        </tr>
+                        <tr>
+                            <th class="text-center align-middle">Código</th>
+                            <th class="text-center align-middle">Nombre</th>
+                            <th class="text-center align-middle">Descripción</th>
+
+                            <th class="text-center align-middle">Variante</th>
+                            <th class="text-center align-middle">Presentacion</th>
+
+                            <th class="text-center align-middle">Precio Real</th>
+                            <th class="text-center align-middle">Precio Descuento</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($data->filter(function($item) {
+                        return !$item->activo;
+                        })->all() as $item)
+                        <tr>
+                            <td class="text-left align-middle">{{ $item->escuela->establecimiento }}</td>
+
+                            <td class="text-center align-middle">
+                                <img src="{{ $item->producto->getPictureAttribute() }}"
+                                    alt="{{ $item->producto->codigo }}" width="100px" class="img img-thumbnail">
+                            </td>
+
+                            <td class="text-center align-middle">{{ $item->producto->codigo }}</td>
+                            <td class="text-left align-middle">{{ $item->producto->nombre }}</td>
+                            <td class="text-left align-middle">{!! $item->producto->descripcion !!}</td>
+
+                            <td class="text-center align-middle">{{ $item->variante->nombre }}</td>
+                            <td class="text-center align-middle">{{ $item->presentacion->nombre }}</td>
+
+                            <td class="text-right align-middle">{{ number_format($item->precio_original,2) }}</td>
+                            <td class="text-right align-middle">{{ number_format($item->precio,2) }}</td>
+
+                            <td class="text-center align-middle">{{ date('d/m/Y H:i:s', strtotime($item->created_at))
+                                }}</td>
                         </tr>
                         @endforeach
                     </tbody>
